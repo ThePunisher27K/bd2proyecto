6a9dee60a1fe23723e147041f9ac726008aeee74
ThePunisher27K <58076933+ThePunisher27K@users.noreply.github.com>
2023-11-26 11:59:56 -0500

    v1.0

--- createTables.sql
+++ createTables.sql
@@ -1,56 +1,57 @@
-CREATE TABLE TipoUsuarios (
+CREATE TABLE v1_TipoUsuarios (
   tipu_uid NUMBER PRIMARY KEY,
   tipu_nombre VARCHAR2(50)
 );
 
-CREATE TABLE Usuarios (
+CREATE TABLE v1_Usuarios (
   us_id NUMBER PRIMARY KEY,
   us_usuario VARCHAR2(100),
   us_correo VARCHAR2(120),
   us_contra VARCHAR2(100),
   us_freg DATE,
   us_estado VARCHAR2(20),
-  us_tipous NUMBER REFERENCES TipoUsuarios(tipu_uid)
+  us_tipous NUMBER 
 );
-ALTER TABLE Usuarios
+
+ALTER TABLE v1_Usuarios
 ADD CONSTRAINT uq_usuario UNIQUE (us_usuario);
-ALTER TABLE Usuarios
+ALTER TABLE v1_Usuarios
 ADD CONSTRAINT uq_correo UNIQUE (us_correo);
 
-ALTER TABLE Usuarios
+ALTER TABLE v1_Usuarios
 ADD CONSTRAINT fk_tipous
 FOREIGN KEY (us_tipous) 
-REFERENCES TipoUsuarios(tipu_uid);
+REFERENCES v1_TipoUsuarios(tipu_uid);
 
 
 CREATE SEQUENCE seq_usuario START WITH 1 INCREMENT BY 1;
 
-CREATE TABLE TipoDatActor (
+CREATE TABLE v1_TipoDatActor (
   tp_did NUMBER PRIMARY KEY,
   tp_nombre VARCHAR2(50)
 );
 
 
-CREATE TABLE DatActores (
+CREATE TABLE v1_DatActores (
   das_did NUMBER PRIMARY KEY,
   das_uid NUMBER ,
   das_tpdid NUMBER ,
   das_vdato VARCHAR2(255)
 );
-ALTER TABLE DatActores
+ALTER TABLE v1_DatActores
 ADD CONSTRAINT fk_dasusid
 FOREIGN KEY (das_uid) 
-REFERENCES Usuarios(us_id);
+REFERENCES v1_Usuarios(us_id);
 
-ALTER TABLE DatActores
+ALTER TABLE v1_DatActores
 ADD CONSTRAINT fk_dastipoda
 FOREIGN KEY (das_tpdid) 
-REFERENCES TipoDatActor(tp_did);
+REFERENCES v1_TipoDatActor(tp_did);
 
 CREATE SEQUENCE seq_dactores START WITH 1 INCREMENT BY 1;
 
 
-CREATE TABLE Proy (
+CREATE TABLE v1_Proy (
  pro_id NUMBER PRIMARY KEY,
  pro_caid NUMBER ,
  pro_titulo VARCHAR2(255),
@@ -61,34 +62,33 @@ CREATE TABLE Proy (
  pro_fechafi DATE
 );
 
-
-ALTER TABLE Proy
+ALTER TABLE v1_Proy
 ADD CONSTRAINT fk_procaid
 FOREIGN KEY (pro_caid) 
-REFERENCES Usuarios(us_id);
+REFERENCES v1_Usuarios(us_id);
 
 CREATE SEQUENCE seq_proyecto START WITH 1 INCREMENT BY 1;
 
-CREATE TABLE Pro_Sele (
+CREATE TABLE v1_Pro_Sele (
   ps_id NUMBER PRIMARY KEY,
   ps_proid NUMBER,
   ps_actid NUMBER,
   ps_fechasel DATE
 );
 
-ALTER TABLE Pro_Sele
+ALTER TABLE v1_Pro_Sele
 ADD CONSTRAINT fk_psproid
 FOREIGN KEY (ps_proid) 
-REFERENCES Proy(pro_id);
+REFERENCES v1_Proy(pro_id);
 
-ALTER TABLE Pro_Sele
+ALTER TABLE v1_Pro_Sele
 ADD CONSTRAINT fk_psacid
 FOREIGN KEY (ps_actid) 
-REFERENCES Usuarios(us_id);
+REFERENCES v1_Usuarios(us_id);
 
 CREATE SEQUENCE seq_prosel START WITH 1 INCREMENT BY 1;
 
-CREATE TABLE Aud_Proye (
+CREATE TABLE v1_Aud_Proye (
   aup_id NUMBER PRIMARY KEY,
   aup_pro_id NUMBER,
   aup_pro_titulo VARCHAR2(255),
@@ -98,25 +98,25 @@ CREATE TABLE Aud_Proye (
   aup_fechafi DATE
 );
 
-ALTER TABLE Aud_Proye
+ALTER TABLE v1_Aud_Proye
 ADD CONSTRAINT fk_aupproid
 FOREIGN KEY (aup_pro_id) 
-REFERENCES Proy(pro_id);
+REFERENCES v1_Proy(pro_id);
 
 
-CREATE TABLE Aud_DatoActor (
+CREATE TABLE v1_Aud_DatoActor (
   audac_id NUMBER PRIMARY KEY,
   audac_uid NUMBER,
-  audac_tpdid NUMBER REFERENCES TipoDatoActor(tp_did),
+  audac_tpdid NUMBER,
   audac_vdato VARCHAR2(255)
 );
 
-ALTER TABLE Aud_DatoActor
+ALTER TABLE v1_Aud_DatoActor
 ADD CONSTRAINT fk_audacuid
 FOREIGN KEY (audac_uid) 
-REFERENCES Usuarios(us_id);
+REFERENCES v1_Usuarios(us_id);
 
-ALTER TABLE Aud_DatoActor
+ALTER TABLE v1_Aud_DatoActor
 ADD CONSTRAINT fk_audactpid
 FOREIGN KEY (audac_tpdid)  
-REFERENCES TipoUsuarios(tipu_uid);
+REFERENCES v1_TipoDatActor(tp_did);
